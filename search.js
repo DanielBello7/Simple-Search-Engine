b8dd30681c2c5894c2f02d8397e9e3e02d1d97b4
Daniel Bello <gokebello@gmail.com>
2021-10-05 11:31:09 +0100

    Complete Project
    Complete Project already done locally

---
+++ search.js
@@ -0,0 +1,24 @@
+
+
+
+const data = require('./data.json')
+
+function query_check(query){
+    let results = []
+
+    data.forEach(item => {
+        const data_name = item.name.toString().toLocaleLowerCase().trim()
+        const query_name = query.toString().toLocaleLowerCase().trim()
+        const input_count = query_name.length
+    
+        let results_box = ""
+        for(i = 0; i < input_count; i++) results_box += data_name[i]
+    
+        if(results_box === query_name) results.push(item)
+    })
+    
+    return results
+}
+
+
+module.exports = query_check
